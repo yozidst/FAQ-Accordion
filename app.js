29f477a49e8d3241e94c4e29aa497a2932de59b1
Yonten <tdabtsa@yahoo.com>
2023-12-15 02:00:13 -0500

    Update

---
+++ app.js
@@ -0,0 +1,16 @@
+const sections = document.querySelectorAll(".questions");
+
+function button(index) {
+    const plusIcon = document.querySelector(`#button${index}`);
+    plusIcon.src = plusIcon.src.includes('plus') ? './assets/images/icon-minus.svg' : './assets/images/icon-plus.svg';
+
+}
+
+sections.forEach((section, index) => {
+    section.addEventListener('click', () => {
+      button(index)
+      section.classList.toggle('active')
+    })
+  })
+
+
